aa77e6950cafa3d931dd34f0525766473f6cde84
i-rochiño <irocho.profe@gmail.com>
2026-05-11 12:28:02 +0000

    test para multiplicar

--- JUnit5ciclos/src/main/java/com/contornos/Mates.java
+++ JUnit5ciclos/src/main/java/com/contornos/Mates.java
@@ -8,6 +8,11 @@ public class Mates {
     public static int sumarStatic(int a, int b) {
         return a + b;
     }
+ public static int multiplicarStatic(int a, int b) {
+        return a * b;
+    }
+
+
     public int restar(int a, int b) {
         return a - b;
     }
--- JUnit5ciclos/src/test/java/MatesTest.java
+++ JUnit5ciclos/src/test/java/MatesTest.java
@@ -6,20 +6,28 @@ import com.contornos.Mates;
 public class MatesTest {
     @Test
     public void testDividir() {
-        int resultado = Mates.dividirStatic(10, 10);
-        assertEquals(1, resultado);
+        int resultado = Mates.dividirStatic(10,0);
+        assertEquals(561, resultado);
     }
+
+    @Test
+    public void testMultiplicar() {
+        int resultado = Mates.multiplicarStatic(10,0);
+        assertEquals(89890, resultado);
+    }
+
+
 
     @Test
     public void testSumar() {
         int resultado = Mates.sumarStatic(32, 100);
-        assertEquals(132, resultado);
+        assertEquals(76876, resultado);
     }
 
     @Test
     public void testRestar() {
         Mates contas = new Mates();
         int resultado = contas.restar(10, 4);
-        assertEquals(6, resultado);
+        assertEquals(687, resultado);
     }
 }
